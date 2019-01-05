75ddf1f1f210746423ca41d03b9aa90de1a7fca3
KaannestadPanda <league@iMac-7.attlocal.net>
2019-01-05 12:56:10 -0800

    calculator, latest tweet

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,10 +12,18 @@ public class Calculator implements ActionListener {
 	double num2;
 	double num3;
 	static Dimension d = new Dimension(100,25);
+	static Dimension g = new Dimension(600,100);
 JButton multbutton = new JButton("mult");
 JButton divbutton = new JButton("div");
 JButton addbutton = new JButton("add");
 JButton subbutton = new JButton("sub");
+
+JTextField field = new JTextField();
+
+JTextField field2 = new JTextField();
+
+JTextField answerfield = new JTextField();
+
 
 	public static void main(String[] args) {
 		Calculator a = new Calculator();
@@ -24,18 +32,18 @@ a.build();
 		
 	}
 	void build() {
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("yeet");
 		JPanel panel = new JPanel();
-		JTextField field = new JTextField();
-		field.setPreferredSize(d);
-		JTextField field2 = new JTextField();
-		field2.setPreferredSize(d);
-		JTextField answerfield = new JTextField();
-		answerfield.setText("answer:");
-		answerfield.setPreferredSize(d);
+		
 		frame.add(panel);
+		
+		field.setPreferredSize(d);
+		field2.setPreferredSize(d);
+		answerfield.setText("answer");
+		answerfield.setPreferredSize(d);
 		panel.add(field);
 		panel.add(field2);
+		
 		multbutton.addActionListener(this);
 		divbutton.addActionListener(this);
 		addbutton.addActionListener(this);
@@ -44,37 +52,50 @@ a.build();
 		panel.add(divbutton);
 		panel.add(addbutton);
 		panel.add(subbutton);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.add(answerfield);
+		frame.setPreferredSize(g);
 		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 
-	double mult() {
+	void doTheDub() {
+		String string1 = field.getText();
+		double number1 =Double.parseDouble(string1);
+		num1=number1;
+		
+		String string2= field2.getText();
+		double number2 =Double.parseDouble(string2);
+		num2=number2;
+	}
+	
+	void mult() {
 		 num3 = num1 * num2;
-		return num3;
+		answerfield.setText(num3+" ");
 
 	}
 
-	double div() {
+	void div() {
 		 num3 = num1 / num2;
-		return num3;
+		 answerfield.setText(num3+" ");
 
 	}
 
-	double add() {
+	void add() {
 		 num3 = num1 += num2;
-		return num3;
+		 answerfield.setText(num3+" ");
 
 	}
 
-	double sub() {
+	void sub() {
 		 num3 = num1 -= num2;
-		return num3;
+		 answerfield.setText(num3+" ");
 
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		doTheDub();
 		JButton checkbutton = (JButton) e.getSource();
 		if(checkbutton==multbutton) {
 			mult();
---
+++ src/LatestTweet.java
@@ -0,0 +1,75 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class LatestTweet implements ActionListener {
+	JTextField text = new JTextField();
+	Dimension d=new Dimension(100,25);
+	public static void main(String[] args) {
+		LatestTweet a = new LatestTweet();
+		a.makeUI();
+	
+
+	
+	}
+	void makeUI() {
+		text.setPreferredSize(d);
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		JButton button=new JButton();
+		
+		button.addActionListener(this);
+		panel.add(text);
+		panel.add(button);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Tweet, Tweet");
+		getLatestTweet(text.getText());
+	}
+	
+	
+	
+	
+	private String getLatestTweet(String searchingFor) {
+
+	      Twitter twitter = new TwitterFactory().getInstance();
+
+	      AccessToken accessToken = new AccessToken(
+	            "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+	            "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+
+	      twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+	            "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+
+	      twitter.setOAuthAccessToken(accessToken);
+
+	      Query query = new Query(searchingFor);
+	      try {
+	            QueryResult result = twitter.search(query);
+	            return result.getTweets().get(0).getText();
+	      } catch (Exception e) {
+	            System.err.print(e.getMessage());
+	            return "What the heck is that?";
+	      }
+	}
+	
+	
+	
+}
